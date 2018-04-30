52c0c57fbd039045a6a6dc9c8e50c8e772e5e910
Kenta Saito <kenta.saito@sagittar.org>
2018-04-30 16:09:15 +0900

    2018-04-30 16:09:15

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -14,7 +14,10 @@ class Welcome
 		// Database schema handler
 		load_library('Tables/TablesJson', 'tables_json_database_schema', FCPATH.'/application/database_schemas');
 		load_library('TablesDatabaseSchema', 'tables_database_schema', lib('db'));
-		load_library('Tables/Tables', 'database_schema_handler', [lib('tables_json_database_schema'), lib('tables_database_schema')]);
+		load_library('Tables/Tables', 'database_schema_handler', [
+			lib('tables_json_database_schema'),
+			lib('tables_database_schema')
+		]);
 
 		// Database schema
 		$tables = array_column(lib('db')->query("SELECT `TABLE_NAME` FROM `information_schema`.`TABLES` WHERE `TABLE_SCHEMA` = '".lib('db')->database."' AND `TABLE_TYPE` = 'BASE TABLE'")->fetch_all(), 0);
@@ -25,7 +28,10 @@ class Welcome
 		// Application schema handler
 		load_library('Tables/TablesJson', 'tables_json_application_schema', FCPATH.'/application/application_schemas');
 		load_library('TablesApplicationSchema', 'tables_application_schema', lib('db'), lib('database_schema_handler'));
-		load_library('Tables/Tables', 'application_schema_handler', [lib('tables_json_application_schema'), lib('tables_application_schema')]);
+		load_library('Tables/Tables', 'application_schema_handler', [
+			lib('tables_json_application_schema'),
+			lib('tables_application_schema'),
+		]);
 
 		// Application schema
 		$tables = array_column(lib('db')->query("SELECT `TABLE_NAME` FROM `information_schema`.`TABLES` WHERE `TABLE_SCHEMA` = '".lib('db')->database."' AND `TABLE_TYPE` = 'BASE TABLE'")->fetch_all(), 0);
@@ -45,17 +51,23 @@ class Welcome
 //		shell_exec('unzip -cq '.__DIR__.'/../misc/third_party/sakila/sakila.dump.zip | mysql -uroot');
 		shell_exec('rm -r '.FCPATH.'/application');
 		mkdir(FCPATH.'/application');
-		load_library('Db', 'db', 'localhost', 'root', '', 'employees');
+		load_library('Db', 'db', 'localhost', 'root', '', 'sakila');
 
 		// Database schema handler
 		load_library('Tables/TablesJson', 'tables_json_database_schema', FCPATH.'/application/database_schemas');
 		load_library('TablesDatabaseSchema', 'tables_database_schema', lib('db'));
-		load_library('Tables/Tables', 'database_schema_handler', [lib('tables_json_database_schema'), lib('tables_database_schema')]);
+		load_library('Tables/Tables', 'database_schema_handler', [
+			lib('tables_json_database_schema'),
+			lib('tables_database_schema'),
+		]);
 
 		// Aliases handler
 		load_library('Tables/TablesJson', 'tables_json_aliases', FCPATH.'/application');
 		load_library('TablesAliases', 'tables_aliases', lib('db'), lib('database_schema_handler'));
-		load_library('Tables/Tables', 'aliases_handler', [lib('tables_json_aliases'), lib('tables_aliases')]);
+		load_library('Tables/Tables', 'aliases_handler', [
+			lib('tables_json_aliases'),
+			lib('tables_aliases'),
+		]);
 
 		// Application schema handler
 		load_library('Tables/TablesJson', 'tables_json_application_schema', FCPATH.'/application/application_schemas');
