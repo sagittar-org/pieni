d47a052bb509854b3ee57660227201f52dd13e01
Kenta Saito <kenta.saito@sagittar.org>
2018-05-01 16:32:54 +0900

    2018-05-01 16:32:54

--- src/TablesApplicationSchema.php
+++ src/TablesApplicationSchema.php
@@ -5,29 +5,11 @@ class TablesApplicationSchema
 		'scalars' => [
 			'value',
 		],
-		'primary_keys' => [
-		],
-		'children' => [
-		],
-		'parents' => [
-			'column',
-			'parent_table',
-			'parent_column',
-			'update_rule',
-			'delete_rule',
-		],
-		'columns' => [
-			'type',
-			'nullable',
-			'default',
-			'extra',
-			'comment',
-		],
 		'join' => [
 			'table',
 			'on',
 		],
-		'append' => [
+		'columns' => [
 			'expr',
 			'after',
 		],
@@ -43,13 +25,12 @@ class TablesApplicationSchema
 
 	public function mtime($name)
 	{
-		return time();
+		return 0;
 	}
 
 	public function get($columns, $name)
 	{
 		$database_schema = $this->database_schema_handler->get($name);
-		$data = $database_schema;
 		$data['scalars']['display'] = ['value' => "`_alias_`.`".array_keys($database_schema['columns'])[0].'`'];
 		foreach ($database_schema['columns'] as $column_key => $column) {
 			if (!in_array($column_key, array_keys($database_schema['primary_keys'])) && preg_match('/char/', $column['type'])) {
@@ -58,11 +39,11 @@ class TablesApplicationSchema
 			}
 		}
 		$data['join'] = [];
-		$data['append'] = [];
+		$data['columns'] = [];
 		$data['hidden'] = [];
 		foreach ($database_schema['parents'] as $parent_key => $parent) {
 			$data['join'][$parent_key] = ['table' => $parent['parent_table'], 'on' => "`{$parent_key}`.`{$parent['parent_column']}` = `{$name}`.`{$parent['column']}`"];
-			$data['append'][$parent_key] = ['expr' => '', 'after' => $parent['column']];
+			$data['columns'][$parent_key] = ['expr' => '', 'after' => $parent['column']];
 			$data['hidden'][$parent['column']] = [];
 		}
 		return $data;
