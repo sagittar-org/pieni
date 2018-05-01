6c36e9797b7e1f7aba3a10d2e7d3df661cf22020
Kenta Saito <kenta.saito@sagittar.org>
2018-05-01 14:30:26 +0900

    2018-05-01 14:30:26

--- views/welcome/prototyping_console/tables.php
+++ views/welcome/prototyping_console/tables.php
@@ -1,6 +1,6 @@
 			<div id="tableTables">
-				<span>
-					<input name="table_name" type="radio">
+				<span class="hide">
+					<input name="table_name" type="radio" disabled>
 					<label name="id"></label>
 				</span>
 			</div>
@@ -10,11 +10,18 @@
 				dataType: 'json',
 				success: (vars) => {
 					let rowElementTemplate = $('#tableTables *:first');
+					let index = 0;
 					for (let rowName in vars.data.scalars) {
 						if (vars.data.scalars[rowName].table !== rowName) continue;
 						let rowElement = rowElementTemplate.clone();
+						rowElement.removeClass('hide');
+						rowElement.find('[type="radio"]').prop('disabled', false);
+						if (index === 0) {
+							rowElement.find('[type="radio"]').prop('checked', true);
+						}
 						rowElement.find('[name="id"]').text(rowName);
 						$('#tableTables').append(rowElement);
+						index++;
 					}
 				},
 			});
