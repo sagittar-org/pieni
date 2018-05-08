c228dd54d40d557c6f2750ced9ded434a9227914
Kenta Saito <kenta.saito@sagittar.org>
2018-05-08 14:51:28 +0900

    2018-05-08 14:51:28

--- controllers/Crud.php
+++ controllers/Crud.php
@@ -1,12 +1,26 @@
 <?php
 class Crud extends \pieni\Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->request_table = g('request_table')->get(g('req')->actor.'.'.g('req')->class.'.'.g('req')->method);
+		$this->table = g('req')->class;
+	}
+
 	public function index()
 	{
+		$query = "
+			SELECT
+				*
+			FROM `{$this->table}`
+			LIMIT 10
+		";
+		$this->vars['rows'] = g('db')->query($query)->fetch_all(MYSQLI_ASSOC);
 		return $this->vars;
 	}
 
-	public function view()
+	public function view($id)
 	{
 		return $this->vars;
 	}
--- views/crud/index.php
+++ views/crud/index.php
@@ -1,3 +1,4 @@
 <div class="container">
   <h2>index</h2>
+<?php r($vars); ?>
 </div>
