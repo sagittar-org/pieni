8742f09bc1dc2a2cb9e0b35dc19c38c85845bcee
Kenta Saito <kenta.saito@sagittar.org>
2018-05-10 18:43:13 +0900

    2018-05-10 18:43:13

--- views/crud/index.php
+++ views/crud/index.php
@@ -1,24 +1,26 @@
 <div class="container">
   <h2><?php h($vars['alias']); ?></h2>
-  <table class="table table-sm">
-    <thead>
-      <tr>
+  <div class="table-responsive">
+    <table class="table table-sm">
+      <thead>
+        <tr>
 <?php foreach ($vars['request_table']['columns'] as $column_name => $column): ?>
-        <th><?php h($column_name); ?></th>
+          <th><?php h($column_name); ?></th>
 <?php endforeach; ?>
-        <th>actions</th>
-      <tr>
-    </thead>
-    <tbody id="<?php h($vars['elm_id']); ?>-data">
-      <tr class="d-none">
+          <th>actions</th>
+        <tr>
+      </thead>
+      <tbody id="<?php h($vars['elm_id']); ?>-data">
+        <tr class="d-none">
 <?php foreach ($vars['request_table']['columns'] as $column_name => $column): ?>
-        <td name="<?php h($column_name); ?>"></td>
+          <td name="<?php h($column_name); ?>"></td>
 <?php endforeach; ?>
-        <td>
-          <a class="action-view" data-href="<?php href("{$vars['alias']}/view"); ?>">view</a>
-        </td>
-      <tr>
-    </tbody>
-  </table>
+          <td>
+            <a class="action-view" data-href="<?php href("{$vars['alias']}/view"); ?>">view</a>
+          </td>
+        <tr>
+      </tbody>
+    </table>
+  </div>
   <script>drawIndex('<?php href($vars['alias'], ['type' => 'api']); ?>', '<?php h($vars['elm_id']); ?>-data');</script>
 </div>
